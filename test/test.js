6d9be32dfaba2aa14f24129816efa11098c8fc91
Kevin Martensson <kevinmartensson@gmail.com>
2014-01-17 14:16:12 +0100

    Add ability to create a hash from Buffers

--- test/test.js
+++ test/test.js
@@ -2,12 +2,18 @@
 'use strict';
 
 var assert = require('assert');
+var fs = require('fs');
 var hashfile = require('../hashfile');
 var path = require('path');
 
 describe('hashfile()', function () {
-    it('should return a hashed file name', function (cb) {
+    it('should return a hashed file name from a String', function (cb) {
         var src = path.join(__dirname, 'fixtures/test.jpg');
+        cb(assert.equal(hashfile(src), 'ac8b2c4b75b2d36988c62b919a857f1baacfcd4c'));
+    });
+
+    it('should return a hashed file name from a Buffer', function (cb) {
+        var src = new Buffer(fs.readFileSync(path.join(__dirname, 'fixtures/test.jpg')));
         cb(assert.equal(hashfile(src), 'ac8b2c4b75b2d36988c62b919a857f1baacfcd4c'));
     });
 });
