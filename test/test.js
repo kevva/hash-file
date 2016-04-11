8a19e321b0da0e7b7bc3d658ba9fdd05f66a0473
Kevin Martensson <kevinmartensson@gmail.com>
2016-04-11 17:26:23 +0200

    Bump AVA

--- test/test.js
+++ test/test.js
@@ -1,36 +1,22 @@
-'use strict';
-var fs = require('fs');
-var path = require('path');
-var test = require('ava');
-var hashFile = require('../');
-var src = path.join(__dirname, 'fixtures/test.jpg');
+import fs from 'fs';
+import path from 'path';
+import test from 'ava';
+import fn from '../';
 
-test('return a hashed file name from a string', function (t) {
-	t.plan(1);
+const src = path.join(__dirname, 'fixtures/test.jpg');
 
-	hashFile(src).then(function (hash) {
-		t.assert(hash === '1bc522c89a9cd36ee274b2e0b2c8bc91d2760c46', hash);
-	});
+test('return a hashed file name from a string', async t => {
+	t.is(await fn(src), '1bc522c89a9cd36ee274b2e0b2c8bc91d2760c46');
 });
 
-test('return a hashed file name from a Buffer', function (t) {
-	t.plan(1);
-
-	hashFile(new Buffer(fs.readFileSync(src))).then(function (hash) {
-		t.assert(hash === '1bc522c89a9cd36ee274b2e0b2c8bc91d2760c46', hash);
-	});
+test('return a hashed file name from a Buffer', async t => {
+	t.is(await fn(new Buffer(fs.readFileSync(src))), '1bc522c89a9cd36ee274b2e0b2c8bc91d2760c46');
 });
 
-test('synchronously return a hashed file name from a string', function (t) {
-	var hash = hashFile.sync(src);
-
-	t.assert(hash === '1bc522c89a9cd36ee274b2e0b2c8bc91d2760c46', hash);
-	t.end();
+test('synchronously return a hashed file name from a string', t => {
+	t.is(fn.sync(src), '1bc522c89a9cd36ee274b2e0b2c8bc91d2760c46');
 });
 
-test('synchronously return a hashed file name from a Buffer', function (t) {
-	var hash = hashFile.sync(new Buffer(fs.readFileSync(src)));
-
-	t.assert(hash === '1bc522c89a9cd36ee274b2e0b2c8bc91d2760c46', hash);
-	t.end();
+test('synchronously return a hashed file name from a Buffer', t => {
+	t.is(fn.sync(new Buffer(fs.readFileSync(src))), '1bc522c89a9cd36ee274b2e0b2c8bc91d2760c46');
 });
